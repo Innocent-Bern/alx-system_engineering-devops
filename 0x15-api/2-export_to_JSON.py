ece065c6dd47be3259b0abbe08ec3fbef9d3bca4
Innocent_Bern <innocentbern22@gmail.com>
2023-10-25 23:23:15 +0300

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+module that interacts with JSON server
+"""
+
+
+import json
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos'
+    todos_res = requests.get(todos_url)
+    user_url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/'
+    user_res = requests.get(user_url)
+
+    user = json.loads(user_res.content)
+    todos = json.loads(todos_res.content)
+
+    uid = user.get('id')
+    u_name = user.get('username')
+    t_array = []
+
+    for item in todos:
+        title = item.get('title')
+        status = item.get('completed')
+        t_array.append({"task": title, "complete": status, "username": u_name})
+
+    with open(f"{uid}.json", "w") as outfile:
+        json.dump({uid: t_array}, outfile)
